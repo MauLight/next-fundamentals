ca5adc828483f6314265a5b99a68257f508323c7
Mau Ulisse Luz <maulisseluz@gmail.com>
2025-03-07 02:19:03 -0300

    feat: create pages route
    file router with nested routes

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+    return (
+        <div className="w-full min-h-screen flex justify-center items-center">
+            <h1>Invoices is in construction...</h1>
+        </div>
+    )
+}
