344fb4e2519937795fcac8737958cc44b4cd6b6f
Mau Ulisse Luz <maulisseluz@gmail.com>
2025-03-10 12:37:46 -0300

    feat: add streaming strategy
    loading.tsx and Suspense localized

--- app/dashboard/page.tsx
+++
@@ -1,7 +0,0 @@
-export default function Page() {
-    return (
-        <div className="w-full min-h-screen flex justify-center items-center">
-            <h1>Dashboard is in construction...</h1>
-        </div>
-    )
-}
