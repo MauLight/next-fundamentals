9f58a129a0546cfa5801bd5810dc11efe4a928fc
Mau Ulisse Luz <maulisseluz@gmail.com>
2025-03-12 17:47:56 -0300

    feat: add metadata to routes

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,9 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: 'Customers'
+}
+
 export default function Page() {
     return (
         <div className="w-full min-h-screen flex justify-center items-center">
